cf13f4fb661fb7bb22fea26ea4bc5f8e29031342
linjianghe <linjianghe@tencent.com>
2018-10-02 20:37:45 +0800

    feat: 新增 recommend mock 数据

--- matman-app/mock_server/mockers/get_recommend/mock_modules/success/index.js
+++ matman-app/mock_server/mockers/get_recommend/mock_modules/success/index.js
@@ -2,6 +2,26 @@ const { getSuccess } = require('../../base');
 
 module.exports = function () {
     return getSuccess({
-        list: []
+        list: [{
+            anchorName: '欢欢💕唱歌的妹子✨',
+            roomId: 1210780249,
+            online: 1,
+            pic: 'https://pic.url.cn/hy_personal_room/1210780249/12107802491536323806/640'
+        }, {
+            anchorName: '我的名字很长啊我的名字很长啊我的名字很长啊我的名字很长啊',
+            roomId: 1061364424,
+            online: 744,
+            pic: 'https://pic.url.cn/hy_personal_room/90481424/904814241531224196/640'
+        }, {
+            anchorName: '可樂☘️安静唱歌',
+            roomId: 1211052048,
+            online: 9999,
+            pic: 'https://pic.url.cn/hy_personal_room/1211052048/12110520481536398310/640'
+        }, {
+            anchorName: '我的小白狐狸',
+            roomId: 1218375629,
+            online: 10086,
+            pic: 'https://pic.url.cn/hy_personal_room/1218375629/12183756291530694005/640'
+        }]
     });
 };
