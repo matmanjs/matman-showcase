36ebb219e5fdb44327a306bc3422bdc78499281d
linjianghe <linjianghe@tencent.com>
2018-10-03 17:20:50 +0800

    feat: 尝试使用websocket来请求数据

--- src/pages/stub-async/business/jsbridge-util/index.js
+++ src/pages/stub-async/business/jsbridge-util/index.js
@@ -1,6 +1,20 @@
+import StubAsyncClient from './stub-async-client';
+
 export function sendRequest(params, callback) {
-    // 模拟异步返回
-    setTimeout(() => {
-        callback(Object.assign({ retcode: 0 }, params));
-    }, 100);
+    // // 模拟异步返回
+    // setTimeout(() => {
+    //     callback(Object.assign({ retcode: 0 }, params));
+    // }, 100);
+
+    let host = 'localhost:9527';
+    let actualURL = '/stub/a/b/stub_get_matman';
+    let query;
+
+    new StubAsyncClient(`http://${host}`).emit(actualURL, query, (data) => {
+        if (process.env.NODE_ENV !== 'production') {
+            console.log(`url=${actualURL}`, query, data);
+        }
+
+        callback(Object.assign({ retcode: 0 }, data));
+    });
 }
