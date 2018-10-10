022b97a0fd0534401c1d2c3f282eea87d0bd16c4
linjianghe <linjianghe@tencent.com>
2018-10-10 22:04:23 +0800

    refactor: 优化初始 async mock

--- src/pages/stub-async/business/matman-stub/index.js
+++ src/pages/stub-async/business/matman-stub/index.js
@@ -3,29 +3,20 @@ import matmanMockAsync from 'matman-mock-async';
 import AsyncRequestMatmanInfo from './async-request-matman-info';
 import AsyncReceivePush from './async-receive-push';
 
-// url 参数中必须有stub_ip=xxx，指定websocket服务器的地址
-// export const STUB_IP = location.search
-//     .match(new RegExp('(\\?|&)stub_ip=([^&]*)(&|$)'))
-//     ? decodeURIComponent(RegExp.$2) : '';
-export const STUB_IP = '127.0.0.1:9527';
-// export const STUB_IP = '10.66.67.146:9527';
-// 可以使用代理的方式来代理到指定的端口上
-// export const STUB_IP = 'local.matmanjs.org';
-
 export default function init() {
-    if (!STUB_IP) {
-        console.log('[matman-stub] no stub ip');
-        return;
-    }
+    // if (window.location.search.indexOf('matman_async') < 0) {
+    //     console.log('No need to start matman async mock!');
+    //     return;
+    // }
 
-    // 创建 stub 客户端对象
-    // TODO 如果是用代理的方式在手机端调试，则必须使用 ip 的方式访问，不能够使用 localhost 和 127.0.0.1
-    // const matmanStubClient = new MatmanStubClient('http://127.0.0.1:3000');
-    // const matmanStubClient = new MatmanStubClient('http://10.66.95.54:3000');
-    const asyncClient = new matmanMockAsync.AsyncClient(STUB_IP);
+    // 如果是用代理的方式在手机端调试，则必须使用 ip 的方式访问，不能够使用 localhost 和 127.0.0.1
+    // const STUB_IP = '127.0.0.1:9527';
+    // const STUB_IP = 'local.matmanjs.org'; 默认值
+    const asyncClient = new matmanMockAsync.AsyncClient();
 
+    // 一次请求一次回调
     new AsyncRequestMatmanInfo(asyncClient).init();
 
+    // 被动接收回调(push)
     new AsyncReceivePush(asyncClient).init();
-
 }
